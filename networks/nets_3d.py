f80947e47f0a115155fbc229e0be75e90bd6129f
Nuno Pimpão Martins <nuno.pimpao@gmail.com>
2020-08-04 16:36:38 +0200

    fixed a typo in 2d unet 1st upsampling block

--- networks/nets_3d.py
+++ networks/nets_3d.py
@@ -22,20 +22,30 @@ def unet_3d(input_shape, conv_kernel_size, filters, last_activation):
 
     block3_conv1 = layers.Conv3D(filters * 4, conv_kernel_size, padding='same', activation='relu')(pool2)
     block3_conv2 = layers.Conv3D(filters * 4, conv_kernel_size, padding='same', activation='relu')(block3_conv1)
+    pool3 = layers.MaxPool3D((2, 2, 2,))(block3_conv2)
 
-    up2 = layers.UpSampling3D((2, 2, 2))(block3_conv2)
-    up2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(up2)
+    block4_conv1 = layers.Conv3D(filters * 8, conv_kernel_size, padding='same', activation='relu')(pool3)
+    block4_conv2 = layers.Conv3D(filters * 8, conv_kernel_size, padding='same', activation='relu')(block4_conv1)
+
+    up3 = layers.UpSampling3D((2, 2, 2))(block4_conv2)
+    up3 = layers.Conv3D(filters * 4, 2, padding='same', activation='relu')(up3)
+    merge3 = layers.Concatenate(axis=-1)([block3_conv2, up3])
+    block5_conv1 = layers.Conv3D(filters * 4, conv_kernel_size, padding='same', activation='relu')(merge3)
+    block5_conv2 = layers.Conv3D(filters * 4, conv_kernel_size, padding='same', activation='relu')(block5_conv1)
+
+    up2 = layers.UpSampling3D((2, 2, 2))(block5_conv2)
+    up2 = layers.Conv3D(filters * 2, 2, padding='same', activation='relu')(up2)
     merge2 = layers.Concatenate(axis=-1)([block2_conv2, up2])
-    block4_conv1 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(merge2)
-    block4_conv2 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(block4_conv1)
+    block6_conv1 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(merge2)
+    block6_conv2 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(block6_conv1)
 
     up1 = layers.UpSampling3D((2, 2, 2))(block4_conv2)
-    up1 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(up1)
+    up1 = layers.Conv3D(filters, 2, padding='same', activation='relu')(up1)
     merge1 = layers.Concatenate(axis=-1)([block1_conv2, up1])
-    block5_conv1 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(merge1)
-    block5_conv2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(block5_conv1)
+    block7_conv1 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(merge1)
+    block7_conv2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(block7_conv1)
 
-    output_tensor = layers.Conv3D(1, (1, 1, 1), padding='same', activation=last_activation)(block5_conv2)
+    output_tensor = layers.Conv3D(1, (1, 1, 1), padding='same', activation=last_activation)(block7_conv2)
 
     model = Model(inputs=input_tensor, outputs=output_tensor)
     return model
