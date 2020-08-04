b407fcbd0b816bb8cca63e00c56ce58d29ff3d4e
Nuno Pimpão Martins <nuno.pimpao@gmail.com>
2020-08-04 11:46:57 +0200

    fix concatenation layer, add normalization to utils

--- networks/nets_3d.py
+++ networks/nets_3d.py
@@ -16,7 +16,7 @@ def unet_3d(input_shape, conv_kernel_size, filters, last_activation):
     block1_conv2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(block1_conv1)
     pool1 = layers.MaxPool3D((2, 2, 2))(block1_conv2)
 
-    block2_conv1 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='reul')(pool1)
+    block2_conv1 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(pool1)
     block2_conv2 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(block2_conv1)
     pool2 = layers.MaxPool3D((2, 2, 2))(block2_conv2)
 
@@ -25,17 +25,17 @@ def unet_3d(input_shape, conv_kernel_size, filters, last_activation):
 
     up2 = layers.UpSampling3D((2, 2, 2))(block3_conv2)
     up2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(up2)
-    merge2 = layers.Concatenate([block2_conv2, up2], axis=-1)
+    merge2 = layers.Concatenate(axis=-1)([block2_conv2, up2])
     block4_conv1 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(merge2)
     block4_conv2 = layers.Conv3D(filters * 2, conv_kernel_size, padding='same', activation='relu')(block4_conv1)
 
     up1 = layers.UpSampling3D((2, 2, 2))(block4_conv2)
     up1 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(up1)
-    merge1 = layers.Concatenate([block1_conv2, up1], axis=-1)
+    merge1 = layers.Concatenate(axis=-1)([block1_conv2, up1])
     block5_conv1 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(merge1)
     block5_conv2 = layers.Conv3D(filters, conv_kernel_size, padding='same', activation='relu')(block5_conv1)
 
     output_tensor = layers.Conv3D(1, (1, 1, 1), padding='same', activation=last_activation)(block5_conv2)
 
-    model = Model(inputs=input_shape, outputs=output_tensor)
+    model = Model(inputs=input_tensor, outputs=output_tensor)
     return model
